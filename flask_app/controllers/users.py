8f4040a422a72f2ce2040d83b096418038d3c75a
tommy bautista <100245701+tommybautista@users.noreply.github.com>
2022-05-31 15:45:07 -0400

    Add files via upload

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, redirect, request_started, session, request, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.event import Event
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html", title='Login')
+
+@app.route('/register')
+def register():
+    return render_template("registration.html", title='Register')    
+
+@app.route('/users/login', methods=['POST'])
+def login_submit():
+    if not User.validate_login(request.form):
+        return redirect('/login')
+
+    user_data = {
+        'email' : request.form['email']
+    }
+    user = User.get_user_by_email(user_data)
+    if user:
+        if not bcrypt.check_password_hash(user.password, request.form['password']):
+            flash('Email/Password combination is incorrect', 'danger')
+            return redirect('/login')
+        session['user_id'] = user.id
+        flash("Login was successful!", 'success')
+        return redirect('/dashboard')
+    flash('Email is not tied to account', 'danger')
+    return redirect ('/login')
+
+@app.route('/users/register', methods=['POST'])
+def registration():
+    if not User.validate_register(request.form):
+        return redirect('/register')
+
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    register_data = {
+        "username" : request.form['username'],
+        "email" : request.form['email'],
+        "password" : pw_hash
+    }
+    user_id = User.register_user(register_data)
+    session['user_id'] = user_id
+    flash("Registration was successful!", 'success')
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    user_data = {
+        "id" : session['user_id']
+    }
+    print(user_data)
+    return render_template('dashboard.html', user = User.get_user_by_id(user_data), events = Event.get_events_by_user(user_data))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    if 'user_id' not in session:
+        flash('You have successfully logged out!', 'success')
+    return redirect('/')
